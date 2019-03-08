f2621c78868d1f54692c829fbec503126c5ebb1d
Noel-SG <45712638+Noel-SG@users.noreply.github.com>
2019-03-08 13:59:31 +0100

    Initial commit

---
+++ ConsoleCalculator/ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Hello!");
+            newLine();
+
+            Console.ReadKey();
+
+        }
+
+        public static void newLine() {
+            Console.WriteLine();
+            Console.Write(">>");
+
+        }
+    }
+}
